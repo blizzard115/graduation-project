1a0dd7b62b3a81474ac793c6df14925cd682ee88
blizzard115 <fubuki.sato@gmail.com>
2026-02-14 17:31:40 +0900

    トップページに投稿画像挿入

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @latest_posts = Post.includes(:user, image_attachment: :blob).order(created_at: :desc).limit(12)
+    @collage_posts = @latest_posts.take(3)
   end
 end
