456d6f237e4867d18806d1144033fbb07c19c6d8
blizzard115 <fubuki.sato@gmail.com>
2026-03-01 16:22:51 +0900

    Run RuboCop

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -10,7 +10,6 @@ require 'active_support/core_ext/integer/time'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_dispatch.host_authorization = nil
-  Rails.logger.debug '✅ loaded config/environments/test.rb'
   # While tests run files are not watched, reloading is not necessary.
   config.enable_reloading = false
 
