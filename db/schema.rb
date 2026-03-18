d2d9af846b328c660c9094637fbaa17393fc1d25
blizzard115 <fubuki.sato@gmail.com>
2026-03-18 10:40:51 +0900

    Add provider and uid to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_03_17_050450) do
+ActiveRecord::Schema[7.1].define(version: 2026_03_18_013503) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -112,7 +112,10 @@ ActiveRecord::Schema[7.1].define(version: 2026_03_17_050450) do
     t.text "profile"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
