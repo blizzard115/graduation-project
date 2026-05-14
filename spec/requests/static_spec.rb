160eadc8fd176e00889142eaf349dd053a4661fa
blizzard115 <fubuki.sato@gmail.com>
2026-05-14 15:02:59 +0900

    static_spec修正

--- spec/requests/static_spec.rb
+++ spec/requests/static_spec.rb
@@ -5,14 +5,14 @@ require 'rails_helper'
 RSpec.describe 'Statics' do
   describe 'GET /terms' do
     it 'returns http success' do
-      get '/static/terms'
+      get '/terms'
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET /privacy' do
     it 'returns http success' do
-      get '/static/privacy'
+      get '/privacy'
       expect(response).to have_http_status(:success)
     end
   end
